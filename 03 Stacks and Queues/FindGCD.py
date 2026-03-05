c99f3c0b559cc5fbb154e59b411a4f3d8ed27205
Yohan Sylva <yohansylva02@gmail.com>
2026-03-05 21:34:45 +0800

    created FindGCD and implemented greatest common denominator euclidean algorithm.

---
+++ 03 Stacks and Queues/FindGCD.py
@@ -0,0 +1,12 @@
+#implements the Eucleidian algorithm to find the greatest common divisor (denominator) of two numbers
+#source: khan academy: https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+def find_gcd(x, y):
+    if y == 0: #base case: if y is 0, then the gcd is x
+        return x
+    else:
+        return find_gcd(y, x % y)
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2:"))
+result = find_gcd(num1, num2)
+print("The greatest common denominator of " + str(num1) + " and " + str(num2) + " is: " + str(result))
