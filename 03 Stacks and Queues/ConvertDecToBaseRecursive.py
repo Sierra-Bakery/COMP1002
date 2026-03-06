41b2d8d6a961730ab1e8279ca3a23ae282514648
Yohan Sylva <yohansylva02@gmail.com>
2026-03-06 10:42:07 +0800

    implemented user input for base 10 number and base to convert to

---
+++ 03 Stacks and Queues/ConvertDecToBaseRecursive.py
@@ -0,0 +1,7 @@
+def BaseToBase(x, y):
+    print('success')
+
+b10 = input("Please enter a BASE 10 Decimal: ")
+convertor = input("Please enter which BASE to convert to") 
+if convertor if y > 2 AND < 16:
+    BaseToBase(x,y):
