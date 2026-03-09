8834caf911dd8c22b49624029965e6a0e9ea3396
Yohan Sylva <yohansylva02@gmail.com>
2026-03-09 23:43:34 +0800

    implemented user input and exception handling

--- 03 Stacks and Queues/FindGCD.py
+++ 03 Stacks and Queues/FindGCD.py
@@ -5,8 +5,14 @@ def find_gcd(x, y):
         return x
     else:
         return find_gcd(y, x % y)
-
-num1 = int(input("Enter number 1: "))
-num2 = int(input("Enter number 2:"))
-result = find_gcd(num1, num2) #calls function 
-print("The greatest common denominator of " + str(num1) + " and " + str(num2) + " is: " + str(result))
+while True: #loops program, can be exited using ctrl c
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    try:
+        result = find_gcd(num1, num2) #calls function 
+        print("The greatest common denominator of " + str(num1) + " and " + str(num2) + " is: " + str(result))
+    except TypeError: #catches invalid data type
+        print("Please enter an integer.")
+    except ValueError: #catches invalid value
+        print("Please enter a valid integer.")
+    
