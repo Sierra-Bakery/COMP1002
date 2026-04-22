251784083d5ce7e1551c712a74cf74976eec3f30
Yohan Sylva <yohansylva02@gmail.com>
2026-04-22 17:49:12 +0800

    implemented insert first method in linked list class

--- 04 Linked Lists/linkedlist.py
+++ 04 Linked Lists/linkedlist.py
@@ -6,8 +6,20 @@ class DSAListNode: #node class, acts as an object used by linked list class, con
 
 class DSALinkedList:
     def __init__(self, value):
-        self.head = None
+        #specifies the first and last node
+        self.head = None 
         self.tail = None
     def isempty(self):
         return self.head is None
-    
+        #returns if the list is empty
+    def insert_first(self, value):
+        new_node = DSAListNode(value)
+        if self.isempty():
+            #means it is the only node in the list, so it is both the head and tail (new node)
+            self.head = new_node
+            self.tail = new_node
+        else:
+            #links new node to the current head
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
